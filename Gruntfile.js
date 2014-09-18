62a8f9009462173ccbb5c3a177610b90c4cb53f7
AlexxHansson <alexxhansson@gmail.com>
2014-09-18 15:03:37 +0200

    grunt file wraps
    Should comment generatedSourceMaps too

--- Gruntfile.js
+++ Gruntfile.js
@@ -164,12 +164,13 @@ module.exports = function (grunt) {
                     },
                     // TODO: Figure out how to make sourcemaps work with grunt-usemin
                     // https://github.com/yeoman/grunt-usemin/issues/30
-                    //generateSourceMaps: true,
+                    generateSourceMaps: true,
                     // required to support SourceMaps
                     // http://requirejs.org/docs/errors.html#sourcemapcomments
                     preserveLicenseComments: false,
                     useStrict: true,
-                    wrap: true
+                    //wrap: true,
+                    //wrapShim: true
                     //uglify2: {} // https://github.com/mishoo/UglifyJS2
                 }
             }
