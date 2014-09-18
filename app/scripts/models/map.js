5e92a9e9b8ed71d27efa6aaa5137c63b3c406764
AlexxHansson <alexxhansson@gmail.com>
2014-09-18 23:05:24 +0200

    Map model fixes

--- app/scripts/models/map.js
+++ app/scripts/models/map.js
@@ -7,19 +7,26 @@ define([
     'use strict';
 
     var MapModel = Backbone.Model.extend({
-        initialize: function() {
-            this.set({map: L.map('map').setView(this.get('coords'), this.get('zoomlevel'))});
-            L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            }).addTo(this.get('map'));
-
-            
-        },
-
         defaults: {
             coords: [56.169401778813686, 14.864437580108644],
             zoomlevel: 16,
-            scrollWheelZoom: false
+            dragging: false,
+            scrollWheelZoom: false,
+            touchZoom: false,
+
+        },
+
+        initialize: function() {
+
+            var options = this.toJSON();
+            this.set({
+                map: L.map('map', options).setView(this.get('coords'), this.get('zoomlevel'))
+            });
+
+            L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+            }).addTo(this.get('map'));
+
         }
     });
 
