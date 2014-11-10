ea3e8b5ece8b53af691e59a4597bfd3d3c3605a8
frebro <frebro@gmail.com>
2014-11-10 15:56:00 +0100

    env example

--- app/scripts/env.example.js
+++ app/scripts/env.example.js
@@ -1,5 +1,6 @@
 $.env = {
     apiReportsUri:  '',     // API URI for Reports
     apiTypesUri:    '',     // API URI for Types
+    apiImagesUri:    '',    // API URI for Images
     fbAppId:        ''      // Facebook application ID
 };
